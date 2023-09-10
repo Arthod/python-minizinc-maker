5c6cd28b436dac70677501c17186e61e95de059d
Ahmad Othman <32404077+Arthod@users.noreply.github.com>
2023-09-10 17:17:33 +0200

    theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,6 +29,7 @@ sys.path.append(os.path.abspath('./test_py_module'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
+    'python_docs_theme',
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
