47c4d477b9656d7ca3793d1a051c147f39bf122e
Arthod <32404077+Arthod@users.noreply.github.com>
2023-09-14 17:56:35 +0200

    minor docstrings

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import pymzm
 import minizinc
 
 model = pymzm.Model()
-n = 5
+n = 2
 q = model.add_variables("q", range(n), val_min=0, val_max=5)
 
-model.add_constraint(pymzm.Expression.NOT(q[2] == (q[0] + 1)))
+model.add_constraint(pymzm.Expression.OR(q[0] == (q[1] + 1), q[0] >= q[1]))
+model.add_constraint(pymzm.Constraint.all_equal(q))
+
 #model.add_constraint(pymzm.Constraint.alldifferent(q))
 #model.add_constraint(pymzm.Constraint.alldifferent([q[i] + i for i in range(n)]))
 #model.add_constraint(pymzm.Constraint.alldifferent([q[i] - i for i in range(n)]))
@@ -15,9 +17,9 @@ model.generate(debug=True)
 model.write("_main.mzn")
 
 # Transform Model into a instance
-#gecode = minizinc.Solver.lookup("gecode")
-#inst = minizinc.Instance(gecode, model)
+gecode = minizinc.Solver.lookup("gecode")
+inst = minizinc.Instance(gecode, model)
 
 # Solve the instance
-#result = inst.solve(all_solutions=True)
-#print(len(result))
+result = inst.solve(all_solutions=True)
+print(result)
