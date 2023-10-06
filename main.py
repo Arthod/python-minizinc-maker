bc7428297058495e09e61fa3c82dc0a4d464997a
Ahmad Othman <32404077+Arthod@users.noreply.github.com>
2023-10-06 15:20:51 +0200

    sequence solve methods, int_search and bool_search

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import minizinc
 
 model = pymzm.Model()
 
-xs = model.add_variables("x", range(10), vtype=pymzm.Variable.VTYPE_INTEGER, val_min=0, val_max=2)
+xs = model.add_variables("x", range(10), vtype=pymzm.Variable.VTYPE_INTEGER, val_min=1, val_max=10)
 
-model.add_constraint(pymzm.Constraint.alldifferent([x - 3 for x in xs]))
+model.add_constraint(pymzm.Constraint.alldifferent(xs))
 model.set_solve_criteria(pymzm.SOLVE_SATISFY)
+model.set_solve_method(pymzm.IntSearch(xs, pymzm.AnnotationVariableChoice.VARCHOICE_FIRST_FAIL, pymzm.AnnotationValueChoice.VALCHOICE_INDOMAIN_MIN))
 
 model.generate(debug=True)
 model.write("out.mzn")
--- src/pymzm/__init__.py
+++ src/pymzm/__init__.py
@@ -9,6 +9,56 @@ SOLVE_MAXIMIZE = "maximize"
 SOLVE_MINIMIZE = "minimize"
 SOLVE_SATISFY = "satisfy"
 
+
+
+class AnnotationVariableChoice:
+    VARCHOICES = [
+        VARCHOICE_INPUT_ORDER, # Choose variables in the order they appear in vars.
+        VARCHOICE_FIRST_FAIL, # Choose the variable with the smallest domain.
+        VARCHOICE_ANTI_FIRST_FAIL, # Choose the variable with the largest domain
+        VARCHOICE_SMALLEST, #Choose the variable with the smallest value in its domain.
+        VARCHOICE_LARGEST, #Choose the variable with the largest value in its domain.
+        VARCHOICE_OCCURRENCE, # Choose the variable with the largest number of attached constraints.
+        VARCHOICE_MOST_CONSTRAINED, # Choose the variable with the smallest domain, breaking ties using the number of constraints.
+        VARCHOICE_MAX_REGRET, # Choose the variable with the largest difference between the two smallest values in its domain.
+        VARCHOICE_DOM_W_DEG, # Choose the variable with the smallest value of domain size divided by weighted degree, where the weighted degree is the number of times the variables been in a constraint which failed
+    ] = [
+        "input_order",
+        "first_fail",
+        "anti_first_fail",
+        "smallest",
+        "largest",
+        "occurrence",
+        "most_constrained",
+        "max_regret",
+        "dom_w_deg",
+    ]
+
+class AnnotationValueChoice:
+    VALCHOICES = [
+        VALCHOICE_INDOMAIN_MIN, # Assign the smallest value in the variable’s domain.
+        VALCHOICE_INDOMAIN_MAX, # Assign the largest value in the variable’s domain.
+        VALCHOICE_INDOMAIN_MIDDLE, # Assign the value in the variable’s domain closest to the mean of its current bounds.
+        VALCHOICE_INDOMAIN_MEDIAN, # Assign the middle value in the variable’s domain.
+        VALCHOICE_INDOMAIN, # Nondeterministically assign values to the variable in ascending order.
+        VALCHOICE_INDOMAIN_RANDOM, # Assign a random value from the variable’s domain.
+        VALCHOICE_INDOMAIN_SPLIT, # Bisect the variable’s domain, excluding the upper half first.
+        VALCHOICE_INDOMAIN_REVERSE_SPLIT, # Bisect the variable’s domain, excluding the lower half first.
+        VALCHOICE_INDOMAIN_INTERVAL, # If the variable’s domain consists of several contiguous intervals, reduce the domain to the first interval. Otherwise just split the variable’s domain.
+    ] = [
+        "indomain_min",
+        "indomain_max",
+        "indomain_middle",
+        "indomain_median",
+        "indomain",
+        "indomain_random",
+        "indomain_split",
+        "indomain_reverse_split",
+        "indomain_interval",
+    ]
+
+
+
 class ValueDict(dict):
     def __iter__(self):
         for v in self.values():
@@ -48,17 +98,52 @@ class ValueDict(dict):
         assert isinstance(other, (int, float, Expression))
         return [s >= other for s in self]
 
-class Method:
-    def __init__(self, s: str):
-        self.s = s
-
+class SeqSearch:
+    def __init__(self, search_annotations: list["SearchAnnotation"]):
+        self.search_annotations = search_annotations
+        if (not all(isinstance(sa, SearchAnnotation) for sa in search_annotations)):
+            raise PymzmInvalidSearchAnnotation()
+        
     def __str__(self):
-        return self.s
+        return f"seq_search([{','.join(str(sa) for sa in self.search_annotations)}])"
 
-    @staticmethod
-    def int_search(arr: List[str], varchoice: str, constrainchoice: str):
-        method = Method(f"int_search({arr}, {varchoice}, {constrainchoice})")
-        return method
+class SearchAnnotation:
+    def __init__(self, search_type: str, variables: List["Variable"], varchoice: str, valchoice: str):
+        self.search_type = search_type
+        self.variables = variables
+
+        self.varchoice = varchoice
+        if (not self.varchoice in AnnotationVariableChoice.VARCHOICES):
+            raise PymzmInvalidVarchoiceAnnotation("varchoice")
+        
+        self.valchoice = valchoice
+        if (not self.valchoice in AnnotationValueChoice.VALCHOICES):
+            raise PymzmInvalidValchoiceAnnotation("valchoice")
+        
+    def __str__(self):
+        return f"{self.search_type}({self.variables}, {self.varchoice}, {self.valchoice})"
+
+class IntSearch(SearchAnnotation):
+    def __init__(self, variables: List["Variable"], varchoice: str, valchoice: str):
+        if (not all(isinstance(v, Variable) for v in variables)):
+            raise PymzmInvalidVariableError("variables", "Atleast one variable is not an integer.")
+        
+        super().__init__("int_search", variables, varchoice, valchoice)
+
+class BoolSearch(SearchAnnotation):
+    def __init__(self, variables: List["VariableBool"], varchoice: str, valchoice: str):
+        if (not all(isinstance(v, VariableBool) for v in variables)):
+            raise PymzmInvalidVariableError("variables", "Atleast one variable is not a bool.")
+        
+        super().__init__("bool_search", variables, varchoice, valchoice)
+
+class SetSearch(SearchAnnotation):
+    def __init__(self, variables: List["Expression"], varchoice: str, valchoice: str):
+        raise NotImplementedError()
+
+class FloatSearch(SearchAnnotation):
+    def __init__(self, variables: List["Expression"], precision: float, varchoice: str, valchoice: str):
+        raise NotImplementedError()
 
 class Expression:
     def __init__(self, name):
@@ -524,7 +609,8 @@ class Model(minizinc.Model):
         else:
             raise Exception(f"Invalid solve criteria: {criteria}")
 
-    def set_solve_method(self, method: Method):
+    def set_solve_method(self, method: SearchAnnotation):
+        assert isinstance(method, (SeqSearch, SearchAnnotation))
         self.solve_method = method
 
     def add_constant(self, name: str, value, vtype=Variable.VTYPE_INTEGER):
@@ -575,18 +661,15 @@ class Model(minizinc.Model):
         self.model_mzn_str += "".join(a._to_mz() for a in self.constants + self.variables + self.constraints)
         
         assert self.solve_criteria is not None
-        # TODO clean this up as to not be spaghetti code.
-        if (self.solve_method is None):
-            if (self.solve_expression is None):
-                self.model_mzn_str += f"solve {self.solve_criteria};\n"
-            else:
-                self.model_mzn_str += f"solve {self.solve_criteria} {self.solve_expression};\n"
-        else:
-            if (self.solve_expression is None):
-                self.model_mzn_str += f"solve :: {self.solve_method} {self.solve_criteria};\n"
-            else:
-                self.model_mzn_str += f"solve :: {self.solve_method} {self.solve_criteria} {self.solve_expression};\n"
+        _solve_method_str = ""
+        if (self.solve_method is not None):
+            _solve_method_str += f":: {self.solve_method} "
+        _solve_method_str += f"{self.solve_criteria}"
+
+        if (self.solve_expression is not None):
+            _solve_method_str += f" {self.solve_expression}"
             
+        self.model_mzn_str += f"solve {_solve_method_str};\n"
         self.add_string(self.model_mzn_str)
         if (debug):
             print(self.model_mzn_str)
@@ -625,7 +708,35 @@ class PymzmNoValues(PymzmException):
         self.argname = argname
 
     def __str__(self):
-        return f"Argument \"{self.argname}, expected iterable but has no values.\""
+        return f"Argument \"{self.argname}\", expected iterable but has no values."
+    
+class PymzmInvalidVarchoiceAnnotation(PymzmException):
+    def __init__(self, argname):
+        self.argname = argname
+
+    def __str__(self):
+        return f"Argument \"{self.argname}\" is not valid as a varchoice annotation."
+    
+class PymzmInvalidValchoiceAnnotation(PymzmException):
+    def __init__(self, argname):
+        self.argname = argname
+
+    def __str__(self):
+        return f"Argument \"{self.argname}\" is not valid as a valchoice annotation."
+    
+class PymzmInvalidVariableError(PymzmException):
+    def __init__(self, argname, msg):
+        self.argname = argname
+        self.msg = msg
+
+    def __str__(self):
+        return f"Argument \"{self.argname}\" error. {self.msg}."
+
+class PymzmInvalidSearchAnnotation(PymzmException):
+    def __init__(self):
+        pass
+    def __str__(self):
+        return f"PymzmInvalidSearchAnnotation."
 
 if __name__ == "__main__":
     pass
