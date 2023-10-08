b99ea1b20e5b94e4ca72127726f5d4e222eaec5e
Ahmad Othman <32404077+Arthod@users.noreply.github.com>
2023-10-08 16:33:19 +0200

    2d array constants

--- src/pymzm/constant.py
+++ src/pymzm/constant.py
@@ -1,27 +1,49 @@
 
 from .variable import *
+from .misc import *
+
+import numpy as np
 
 class Constant:
     def __init__(self, name: str, value, vtype=Variable.VTYPE_INTEGER):
         self.name = name
         self.value = value
-        self.vtype = vtype
-        
         if (self.value is None):
             raise Exception("Non-initialized constant is not supported by pymzm.")
         
+        self.vtype = vtype        
+        if (self.vtype not in [Variable.VTYPE_INTEGER, Variable.VTYPE_BOOL]):
+            raise Exception("Invalid vtype for constant. Currently only integer and boolean types are supported")
+        
+        arr = np.array(value)
+        if (arr.shape):
+            # Is nd array
+            self.shape = arr.shape
+
+        else:
+            # is single value
+            self.shape = None
+        
     def __getitem__(self, other: Expression):
         # TODO boolean expression
-        return Expression(f"{self.name}[{str(other)} + 1]")
+        if (self.shape is None):
+            raise Exception("Constant cant be indexed as it is a single value")
+        
+        if (len(self.shape) == 1):
+            return Expression(f"{self.name}[{str(other)} + 1]")
+        
+        else:
+            return Expression(f"{self.name}[{', '.join(f'{o} + 1' for o in other)}]")
 
     def __str__(self):
         return self.name
     
     def _to_mz(self):
-        if (isinstance(self.value, list)):
-            return f"array[1..{len(self.value)}] of {self.vtype}: {self.name} = {self.value};\n"
-
-        elif (isinstance(self.value, int)):
+        if (self.shape is None):
             return f"{self.vtype}: {self.name} = {self.value};\n"
+        
+        else:
+            mz_array = array_py2mz(self.value, self.shape)
+            return f"array[{','.join(f'1..{d}' for d in self.shape)}] of {self.vtype}: {self.name} = {mz_array};\n"
         
         
--- src/pymzm/misc.py
+++ src/pymzm/misc.py
@@ -3,3 +3,8 @@ from .expression import *
     
 def variableIterable2Str(variables) -> str:
     return str([v.name if isinstance(v, Expression) else v for v in variables]).replace("'", "")
+
+def array_py2mz(arr, shape):
+    assert len(shape) <= 2
+
+    return str(arr).replace("], [", "|").replace("[[", "[|").replace("]]", "|]")
